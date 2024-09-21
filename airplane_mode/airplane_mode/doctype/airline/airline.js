623a61f58692410c560c5e18bfe2cb1f8e85a997
Rijosh <rijosh.dev@gmail.com>
2024-09-21 22:40:01 +0530

    added link to website

--- airplane_mode/airplane_mode/doctype/airline/airline.js
+++ airplane_mode/airplane_mode/doctype/airline/airline.js
@@ -1,8 +1,16 @@
 // Copyright (c) 2024, Rijosh and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Airline", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Airline", {
+	refresh(frm) {
+		if (frm.doc.website) {
+			frm.add_custom_button(
+				__("Go to Website"),
+				function () {
+					window.open(frm.doc.website, "_blank");
+				},
+				__("Links")
+			);
+		}
+	},
+});
