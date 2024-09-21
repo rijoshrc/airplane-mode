7ff90c13340da20cd7cb7b73731fba70c5729e62
Rijosh <rijosh.dev@gmail.com>
2024-09-21 22:43:05 +0530

    updated text

--- airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.js
+++ airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.js
@@ -6,27 +6,24 @@ frappe.ui.form.on("Airplane Ticket", {
 		frm.add_custom_button(
 			"Assign seat",
 			function () {
-				// Create a new dialog
-				let seat_dialog = new frappe.ui.Dialog({
+				let dialog = new frappe.ui.Dialog({
 					title: "Select seat",
 					fields: [
 						{
 							label: "Seat Number",
 							fieldname: "seat_number",
 							fieldtype: "Data",
-							reqd: true, // make this field mandatory
+							reqd: true,
 						},
 					],
-					primary_action_label: "Set",
+					primary_action_label: "Assign",
 					primary_action(values) {
-						// Set the seat number in the form field
 						frm.set_value("seat", values.seat_number);
-						seat_dialog.hide();
+						dialog.hide();
 					},
 				});
 
-				// Show the dialog
-				seat_dialog.show();
+				dialog.show();
 			},
 			__("Actions")
 		);
