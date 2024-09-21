47dac052c05d8afc6cb37a845c05bd14b3a11609
Rijosh <rijosh.dev@gmail.com>
2024-09-21 22:40:12 +0530

    action button for adding seat number

--- airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.js
+++ airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.js
@@ -1,8 +1,34 @@
 // Copyright (c) 2024, Rijosh and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Airplane Ticket", {
-// 	refresh(frm) {
+frappe.ui.form.on("Airplane Ticket", {
+	refresh(frm) {
+		frm.add_custom_button(
+			"Assign seat",
+			function () {
+				// Create a new dialog
+				let seat_dialog = new frappe.ui.Dialog({
+					title: "Select seat",
+					fields: [
+						{
+							label: "Seat Number",
+							fieldname: "seat_number",
+							fieldtype: "Data",
+							reqd: true, // make this field mandatory
+						},
+					],
+					primary_action_label: "Set",
+					primary_action(values) {
+						// Set the seat number in the form field
+						frm.set_value("seat", values.seat_number);
+						seat_dialog.hide();
+					},
+				});
 
-// 	},
-// });
+				// Show the dialog
+				seat_dialog.show();
+			},
+			__("Actions")
+		);
+	},
+});
