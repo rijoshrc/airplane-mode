c4416eecfd0738715a8bc806a4846c71b63a5992
Rijosh <rijosh.dev@gmail.com>
2024-09-22 13:44:09 +0530

    reports and weblink

--- airplane_mode/airplane_mode/doctype/airline/airline.js
+++ airplane_mode/airplane_mode/doctype/airline/airline.js
@@ -4,13 +4,7 @@
 frappe.ui.form.on("Airline", {
 	refresh(frm) {
 		if (frm.doc.website) {
-			frm.add_custom_button(
-				__("Go to Website"),
-				function () {
-					window.open(frm.doc.website, "_blank");
-				},
-				__("Links")
-			);
+			frm.add_web_link(frm.doc.website, __("Go to Website"));
 		}
 	},
 });
