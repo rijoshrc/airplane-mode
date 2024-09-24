a5483965063c4a1cfcbe50a1c6ca46e6329405c4
Rijosh <rijosh.dev@gmail.com>
2024-09-24 14:03:15 +0530

    configuration table for rent

--- airplane_mode/airplane_mode/doctype/airport_shop/airport_shop.py
+++ airplane_mode/airplane_mode/doctype/airport_shop/airport_shop.py
@@ -1,9 +1,15 @@
 # Copyright (c) 2024, Rijosh and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class AirportShop(Document):
+	def validate(self):
+		if not self.rent_amount:
+			rent_config = frappe.get_doc("Shop Rent Configuration")
+			self.rent_amount = rent_config.default_rent_amount
+
+			
 	pass
